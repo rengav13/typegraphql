2055f69278ef1b1b1c7e5ae57a74a9f91a584d88
Vagner Silva <autoos.tecnologia@gmail.com>
2021-08-06 10:00:57 -0300

    Apollo server setup
    - To configure base project
    - To setup apollo server hello world
    - To implement hello world Resolver

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import "reflect-metadata";
+import { buildSchema, Query, Resolver } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+
+    @Query(() => String, { nullable: true, description: "Dummy description" })
+    async hello() {
+        return "Hello world";
+    }
+}
+
+const main = async () => {
+    const schema = await buildSchema({
+        resolvers: [HelloResolver]
+    });
+
+    const apolloServer = new ApolloServer({ schema });
+
+    const app = Express();
+    
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("Server started on http://localhost:4000/graphql");
+    });
+}
+
+main();
