c2b2ec859da6e4ed3e5bc996399eb60f90d11058
Vagner Silva <autoos.tecnologia@gmail.com>
2021-08-06 11:20:03 -0300

    02 Register entity
    - To create entity model
    - To isolate resolver class
    - To implement user mutation
    - To configure typeorm
    - To implement field resolver to add new fields to a type

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
 import "reflect-metadata";
-import { buildSchema, Query, Resolver } from "type-graphql";
-
-@Resolver()
-class HelloResolver {
-
-    @Query(() => String, { nullable: true, description: "Dummy description" })
-    async hello() {
-        return "Hello world";
-    }
-}
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
+    await createConnection();
     const schema = await buildSchema({
-        resolvers: [HelloResolver]
+        resolvers: [RegisterResolver]
     });
 
     const apolloServer = new ApolloServer({ schema });
 
     const app = Express();
-    
+
     apolloServer.applyMiddleware({ app });
 
     app.listen(4000, () => {
