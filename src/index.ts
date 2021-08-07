fced50fb92626d16f8d2ecc96a81296a80e79a9c
Vagner Silva <autoos.tecnologia@gmail.com>
2021-08-07 08:33:50 -0300

    09 Test setup
    - To setup test environment with jest
    - To configure a setup for test database

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,13 @@ import cors from "cors";
 import Express from "express";
 import session from "express-session";
 import "reflect-metadata";
-import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import { redis } from "./redis";
+import { createSchema } from "./utils/createSchema";
 
 const main = async () => {
     await createConnection();
-    const schema = await buildSchema({
-        resolvers: [__dirname + "/modules/**/*.ts"],
-        authChecker: ({ context: { req } }) => {
-            return !!req.session.userId;
-        }
-    });
+    const schema = await createSchema();
 
     const apolloServer = new ApolloServer({
         schema,
---
+++ src/modules/user/register/Register.test.ts
@@ -0,0 +1,45 @@
+import { Connection } from "typeorm";
+import { gCall } from "../../../test-utils/gCall";
+import { testConn } from "../../../test-utils/testConn";
+
+let conn: Connection;
+
+beforeAll(async () => {
+    conn = await testConn();
+});
+
+afterAll(async () => {
+    await conn.close();
+});
+
+const registerMutation = `
+mutation Register($data: RegisterInput!) {
+  register(
+    data: $data
+  ) {
+    id
+    firstName
+    lastName
+    email
+    name
+  }
+}
+`;
+
+describe("Register resolver", () => {
+    it("create user", async () => {
+        console.log(
+            await gCall({
+                source: registerMutation,
+                variableValues: {
+                    data: {
+                        firstName: "bob",
+                        lastName: "bob2",
+                        email: "bob@bob.com",
+                        password: "asdfasdf"
+                    }
+                }
+            })
+        );
+    });
+});
