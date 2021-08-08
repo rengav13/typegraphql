8c532198af3520d8923440c89c60dedc62902771
Vagner Silva <autoos.tecnologia@gmail.com>
2021-08-08 06:13:59 -0300

    12 File upload

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -6,6 +6,7 @@ import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
 import { LoginResolver } from "../modules/user/Login";
 import { LogoutResolver } from "../modules/user/Logout";
 import { MeResolver } from "../modules/user/Me";
+import { ProfilePictureResolver } from "../modules/user/ProfilePicture";
 import { RegisterResolver } from "../modules/user/Register";
 
 export const createSchema = () =>
@@ -19,7 +20,8 @@ export const createSchema = () =>
       MeResolver,
       RegisterResolver,
       CreateUserResolver,
-      CreateProductResolver
+      CreateProductResolver,
+      ProfilePictureResolver
     ],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
